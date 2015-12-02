e992b9b6f0da7e1fe6226abe643a6d82d9559c8f
Derek Crous <ludocrousdev@gmail.com>
2015-12-02 13:00:02 +0000

    Add secret.swift to contain personal API keys

---
+++ VirtualTourist/secret.swift
@@ -0,0 +1,12 @@
+//
+//  secret.swift
+//  VirtualTourist
+//
+//  Created by Derek Crous on 02/12/2015.
+//  Copyright © 2015 Ludocrous Software. All rights reserved.
+//
+
+import Foundation
+
+//This file is on local .gitignore list and only checked in once. Please use your own API Keys here.
+let FLICKER_API_KEY = "API KEY GOES HERE"
