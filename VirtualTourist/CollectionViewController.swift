4d3404c6326921d1b5367b2851f90124643387ce
Derek Crous <ludocrousdev@gmail.com>
2015-12-03 12:45:44 +0000

    Add Photocell class and introduce basic CollectionView functionality

--- VirtualTourist/CollectionViewController.swift
+++ VirtualTourist/CollectionViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import MapKit
 
-class CollectionViewController : UIViewController {
+class CollectionViewController : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var pin: Pin!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+//        let width = CGRectGetWidth(collectionView!.frame) / 3
+//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         setupMapView()
     }
 
@@ -33,5 +38,33 @@ class CollectionViewController : UIViewController {
         FlClient.sharedInstance().getCollectionAroundPin(pin) 
         
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let layout : UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.headerReferenceSize = CGSizeZero
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        let width = floor(collectionView.frame.size.width / 3)
+        layout.itemSize = CGSize(width: width, height: width)
+        collectionView.collectionViewLayout = layout
+    }
+    
+    //MARK: Collection view methods
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 21
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("photo", forIndexPath: indexPath) as! PhotoCell
+        cell.showActivity()
+        return cell
+    }
 }
---
+++ VirtualTourist/PhotoCell.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoCell.swift
+//  VirtualTourist
+//
+//  Created by Derek Crous on 03/12/2015.
+//  Copyright © 2015 Ludocrous Software. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+  
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var activityView : UIActivityIndicatorView?
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        if let activityView = activityView {
+            if activityView.isAnimating() {
+                activityView.stopAnimating()
+            }
+        }
+    }
+
+    func showActivity() {
+        activityView = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
+        activityView?.center = CGPointMake(self.frame.width / 2, self.frame.height / 2)
+        activityView?.color = UIColor.whiteColor()
+        activityView?.startAnimating()
+        imageView.addSubview(activityView!)
+        dbg("ShowActivity called on photocell")
+    }
+}
